1ef835ac77f4962cc220541496bad7b5471832e4
juanjuy <alex.juanjuy@gmail.com>
2022-07-10 17:02:03 -0700

    babel plugins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Client from '../lib/Client.js';
 import Config from '../lib/Config.js';
 import { Provider, Context } from '../lib/Provider.js';
-
-module.exports = { Client, Config, Provider, Context };
+export default {
+  Client,
+  Config,
+  Provider,
+  Context
+};
